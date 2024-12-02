175a2faebcacd02a8cb16438641f7d706bc23b54
Sorokovskys <Sorokovskys@ukr.net>
2024-12-02 22:25:58 +0200

    refactor

--- Database/Core/Repository.cs
+++ Database/Core/Repository.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using Database.Contracts;
 using Database.Storages;
 
--- Database/Events/SuccessEvents.cs
+++ Database/Events/SuccessEvents.cs
@@ -6,7 +6,7 @@ public class SuccessEvents
 
     public event Operation Created;
     public event Operation Removed;
-    
+
     public void OnCreated(object data)
     {
         Created?.Invoke(data);
